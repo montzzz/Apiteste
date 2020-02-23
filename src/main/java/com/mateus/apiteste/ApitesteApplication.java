1a798c74a5020e8cec81ff443e9becb8345634ef
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-02-23 13:05:07 -0300

    Primeiro commit

---
+++ src/main/java/com/mateus/apiteste/ApitesteApplication.java
@@ -0,0 +1,15 @@
+package com.mateus.apiteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitesteApplication.class, args);
+	}
+
+	
+	
+}
