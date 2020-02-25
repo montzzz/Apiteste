d4bd77a8304b8eb33837f32a4a4a92b6c8fc601c
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-02-24 23:47:05 -0300

    Commit com banco h2 funcionando (criando table com annotation)

--- src/main/java/com/mateus/apiteste/resources/CategoriaResource.java
+++ src/main/java/com/mateus/apiteste/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.mateus.apiteste.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mateus.apiteste.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {		
-		return "Rest está ok";		
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(cat2);
+		lista.add(cat2);
+		
+		return lista;		
 	}
 
 	
