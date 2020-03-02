0cd07b7ce9ab7cf3976def877ed5436ac9177603
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-03-02 13:37:48 -0300

    Criado primeiro relacionamento (Produto x Categorias)

--- src/main/java/com/mateus/apiteste/ApitesteApplication.java
+++ src/main/java/com/mateus/apiteste/ApitesteApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mateus.apiteste.domain.Categoria;
+import com.mateus.apiteste.domain.Produto;
 import com.mateus.apiteste.repositories.CategoriaRepository;
+import com.mateus.apiteste.repositories.ProdutoRepository;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 @SpringBootApplication
 public class ApitesteApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApitesteApplication.class, args);
@@ -26,8 +32,19 @@ public class ApitesteApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-				
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));	
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 	
 	
