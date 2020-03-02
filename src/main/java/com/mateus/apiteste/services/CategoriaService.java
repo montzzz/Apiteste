f7f28d4516d09c0c87f7ecd1b9d8c3f03eef4aa6
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-03-02 12:23:50 -0300

    Criado respository e service para Categoria.

---
+++ src/main/java/com/mateus/apiteste/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.mateus.apiteste.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateus.apiteste.domain.Categoria;
+import com.mateus.apiteste.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
