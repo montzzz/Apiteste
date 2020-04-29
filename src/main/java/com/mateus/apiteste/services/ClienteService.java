39744d2e7d3ef3ff16e686dd2a535a045398686b
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-04-28 22:20:32 -0300

    Implementado endpoint para Cliente pelo seu ID.

---
+++ src/main/java/com/mateus/apiteste/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.mateus.apiteste.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateus.apiteste.domain.Cliente;
+import com.mateus.apiteste.repositories.ClienteRepository;
+import com.mateus.apiteste.services.exceptions.ObjectNotfoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotfoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
