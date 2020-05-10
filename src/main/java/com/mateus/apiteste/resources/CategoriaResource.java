ffe7be4afcf7bcf31c41c78be140de05fb403e2c
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-05-10 19:04:47 -0300

    Buscando todas as categorias com DTO (Data Transfer Object).

--- src/main/java/com/mateus/apiteste/resources/CategoriaResource.java
+++ src/main/java/com/mateus/apiteste/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.mateus.apiteste.resources;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mateus.apiteste.domain.Categoria;
+import com.mateus.apiteste.dto.CategoriaDTO;
 import com.mateus.apiteste.services.CategoriaService;
 
 @RestController
@@ -55,5 +57,17 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		// procura todas as categorias e armazena em uma lista
+		List<Categoria> list = service.findAll();
+		// Transforma a lista recebida para o objeto DTO
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		// retorna o DTO
+		return ResponseEntity.ok().body(listDto);
+	}
+		
+	
 	
 }
