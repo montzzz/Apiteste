16324483a6a7d1d07bfc92f30d33a7969ba1c6e6
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-05-17 23:08:12 -0300

    Implementado POST do cliente.

--- src/main/java/com/mateus/apiteste/resources/ClienteResource.java
+++ src/main/java/com/mateus/apiteste/resources/ClienteResource.java
@@ -1,6 +1,6 @@
 package com.mateus.apiteste.resources;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,10 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mateus.apiteste.domain.Cliente;
-import com.mateus.apiteste.domain.Cliente;
 import com.mateus.apiteste.dto.ClienteDTO;
+import com.mateus.apiteste.dto.ClienteNewDTO;
 import com.mateus.apiteste.services.ClienteService;
 
 @RestController
@@ -76,7 +77,16 @@ public class ClienteResource {
 		// retorna o DTO
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
+		return ResponseEntity.created(uri).build();		
+	}
 
 	
 }
--- src/main/java/com/mateus/apiteste/services/ClienteService.java
+++ src/main/java/com/mateus/apiteste/services/ClienteService.java
@@ -9,11 +9,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mateus.apiteste.domain.Categoria;
+import com.mateus.apiteste.domain.Cidade;
 import com.mateus.apiteste.domain.Cliente;
+import com.mateus.apiteste.domain.Endereco;
+import com.mateus.apiteste.domain.enums.TipoCliente;
 import com.mateus.apiteste.domain.Cliente;
 import com.mateus.apiteste.dto.ClienteDTO;
+import com.mateus.apiteste.dto.ClienteNewDTO;
+import com.mateus.apiteste.repositories.CidadeRepository;
 import com.mateus.apiteste.repositories.ClienteRepository;
+import com.mateus.apiteste.repositories.EnderecoRepository;
 import com.mateus.apiteste.services.exceptions.DataIntegrityException;
 import com.mateus.apiteste.services.exceptions.ObjectNotfoundException;
 
@@ -21,12 +29,27 @@ import com.mateus.apiteste.services.exceptions.ObjectNotfoundException;
 public class ClienteService {
 	
 	@Autowired
-	private ClienteRepository repo;
+	private ClienteRepository repo;	
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	
 	public Cliente find(Integer id) {
 		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotfoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEndereco());
+		return obj; 
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -60,9 +83,28 @@ public class ClienteService {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 	
+	public Cliente fromDto(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid.get());
+		cli.getEndereco().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;		
+	}
+	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+
 
 }
