2b45b6c64052b1cdff58de9cd9bdd095bc8d991b
Mateus Monteiro <monteiro.mateust@gmail.com>
2020-06-14 23:12:55 -0300

    Criando Profile de teste.

---
+++ src/main/java/com/mateus/apiteste/Config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.mateus.apiteste.Config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mateus.apiteste.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		// chama o db service para o profile test
+		dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+
+}
